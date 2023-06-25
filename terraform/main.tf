ec790f98f31274c0945cac2f310ec9fde7496435
Juan Pablo Tamayo <jptamayo@gmail.com>
2023-06-25 17:04:57 -0400

    Ansible extendido / carpeta extras con YAML para aplicar en K8s

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ resource "aws_security_group" "k8s-devops-sg" {
   name        = "k8s-devops-sg"
   description = "Security Group for Kubernetes DevOps instance"
 
+  # SSH Access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -9,6 +10,7 @@ resource "aws_security_group" "k8s-devops-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # etcd server client API
   ingress {
     from_port   = 2379
     to_port     = 2380
@@ -16,6 +18,31 @@ resource "aws_security_group" "k8s-devops-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Logstash
+  ingress {
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # web-based interface of Kibana
+  ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Elasticsearch 
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Kubernetes API
   ingress {
     from_port   = 6443
     to_port     = 6443
@@ -23,6 +50,55 @@ resource "aws_security_group" "k8s-devops-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Future port-forward access
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Future port-forward access
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Future port-forward access
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # CoreDNS UDP request
+  ingress {
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Prometheus webadmin port 9090
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Prometheus node-checks port 9100
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Kubelet API
   ingress {
     from_port   = 10250
     to_port     = 10250
@@ -30,6 +106,7 @@ resource "aws_security_group" "k8s-devops-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # kube-controller-manager
   ingress {
     from_port   = 10257
     to_port     = 10257
@@ -37,6 +114,7 @@ resource "aws_security_group" "k8s-devops-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # kube-scheduler
   ingress {
     from_port   = 10259
     to_port     = 10259
@@ -44,6 +122,7 @@ resource "aws_security_group" "k8s-devops-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # NodePort Services
   ingress {
     from_port   = 30000
     to_port     = 32767
@@ -51,6 +130,7 @@ resource "aws_security_group" "k8s-devops-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Full salida
   egress {
     from_port   = 0
     to_port     = 0
@@ -79,6 +159,20 @@ resource "aws_instance" "k8s-node" {
   }
 }
 
-output "ec2_global_ips" {
+resource "aws_instance" "k8s-elk" {
+  ami                    = lookup(var.ec2_ami, var.region)
+  key_name               = "jpdevopsKeyPair"
+  instance_type          = var.instance_type # 
+  vpc_security_group_ids = [aws_security_group.k8s-devops-sg.id]
+  tags = {
+    Name = "k8s-elk"
+  }
+}
+
+output "ec2_K8s-node_ips" {
   value = ["${aws_instance.k8s-node.*.public_ip}"]
+}
+
+output "ec2_ELK_ip" {
+  value = ["${aws_instance.k8s-elk.*.public_ip}"]
 }
